8b6144e2dbcc3ea7ada74f70ab83cd67d8fde16a
DulangiAnuththara <43141556+DulangiAnuththara@users.noreply.github.com>
2019-10-02 22:11:49 +0530

    first commit

---
+++ Testing/main.js
@@ -0,0 +1,21 @@
+let op = 1;
+
+switch (op) {
+
+    case 1:
+        console.log("You have select Kai");
+        break;
+
+    case 2:
+        console.log("You have select Jennie");
+        break;
+
+    case 3:
+        console.log("You have select Krystal");
+        break;
+
+    default:
+        console.log("You are not a kpop fan");
+
+
+};
